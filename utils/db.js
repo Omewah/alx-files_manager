d60bc1e0940c79817ec8601a07ef165fb7a0646a
Omewah <joelomewah@ymail.com>
2024-03-13 07:00:19 +0100

    contains the redis and DB clients

--- utils/db.js
+++ utils/db.js
@@ -6,6 +6,7 @@ const DATABASE = process.env.DB_DATABASE || 'files_manager';
 
 const url = `mongodb://${HOST}:${PORT}`;
 
+
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
